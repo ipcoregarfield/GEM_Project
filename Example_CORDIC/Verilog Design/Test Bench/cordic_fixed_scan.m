c23a8042eb491923b1d371c53c9927cde5db2984
Garfield, Writer of Snorkeling in Verilog Bay <ipcore_garfield@sina.com>
2016-06-26 12:36:46 +0800

    Update cordic_fixed_scan.m

--- Example_CORDIC/Verilog Design/Test Bench/cordic_fixed_scan.m
+++ Example_CORDIC/Verilog Design/Test Bench/cordic_fixed_scan.m
@@ -1,9 +1,10 @@
-function [bit_wide, max_err, results, ek, errs, special_value] = cordic_fixed_scan( step, order, err_limitation, mode, bit_limitation)
+function [bit_wide, max_err, results, ek, errs, special_value] = cordic_fixed_scan( step, order, err_limitation, mode, bit_limitation, sample_angle)
 %Whole angle scan for CORDIC algorithm
 %Input: step: #step in the value domain
 %       order: CORDIC order
 %       err_limitation: allowed maximum error
 %       bit_limitation: allowed maximum bit wide
+%       sample_angle: mid-result sampling angle
 %       mode: CORDIC mode
 %            sin/cos for 1; atan for 2; sqrt for 3;
 %Output: max_err: maximum error
@@ -62,7 +63,11 @@ else
         end
     end
 end
-bit_wide = max_bit;
+if (mode == 1)
+    bit_wide = max_bit+1;
+else
+    bit_wide = max_bit;
+end
 
 %errors calculation
 max_err = 0;
@@ -70,13 +75,28 @@ if (mode(1) ~= 3)
     %single scan value
     for loop1 = 1:step
         w = domain(loop1);
-        [value, real_value, err, mid_results, ek] = cordic_fixed( w, mode, bit_wide, order);
+        [value, real_value, err, mid_results, ek, x0] = cordic_fixed( w, mode, bit_wide, order);
         if ( err > max_err)
             max_err = err;
+        end
+        
+         %sample mid-results at set angle
+        if ( abs(w - sample_angle) < (err_limitation) && (mode == 1) )
+            results.mid_results = mid_results;
+            results.ek = ek;
+            results.x0 = x0;
+            results.angle_err = abs(w - sample_angle);
+        end
+        
+        if (mode == 1)
+            w = mid_results.z(1);
+        else
+            w = mid_results.y(1);
         end
         results.w = [ results.w; w];
         results.x = [ results.x; value];
         errs = [errs, err];
+       
     end
 else
     %two scan values
@@ -88,7 +108,7 @@ else
                 max_err = err;
             end
             errs = [errs, err];
-            results.w = [ results.w; w];
+            results.w = [ results.w; [mid_results.x(1), mid_results.y(1)]];
             results.x = [ results.x; value];
         end
     end
